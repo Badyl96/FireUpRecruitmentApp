c122f17957688548b8ebefad93fd5284f78e6729
Patryk Pietrasiak <pietrasiak.patryk.05@gmail.com>
2020-08-24 20:31:19 +0200

    added routing to main-page

--- front-end/angular-recruitment-app/src/app/app-routing.module.ts
+++ front-end/angular-recruitment-app/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'main-page',
+    component: MainComponent
   },
   {
     path: '**',
