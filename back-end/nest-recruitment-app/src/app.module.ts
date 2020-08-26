4ecc1e0efa214b75e88e24463efd13dc98b8f145
Patryk Pietrasiak <pietrasiak.patryk.05@gmail.com>
2020-08-26 17:23:29 +0200

    added mongos

--- back-end/nest-recruitment-app/src/app.module.ts
+++ back-end/nest-recruitment-app/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './Login/auth.module';
+import { UserModule } from './User/user.module';
 
 @Module({
   imports: [AuthModule],
